61fec5de8674db562228fe53444ee45014806321
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-24 15:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ window.promises = [];
 // Do not change the code above this
 // add your promises to the array `promises`
 // Array of promises
-const promises = Array.from({ length: 5 }, () => {
-  const randomTime = Math.floor(Math.random() * 5) + 1; // Random time between 1 and 5 seconds
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`Promise resolved after ${randomTime} seconds`);
-    }, randomTime * 2000);
-  });
-});
-
-// Using Promise.any()
-Promise.any(promises)
-  .then((result) => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.textContent = result;
-  })
-  .catch((error) => {
-    console.log(error); // Handle error if no promises resolve
-  });
-
+let a = new Promise(resolve => {
+setTimeout(() => { resolve("a")
+}, 3000)
+})
+let b = new Promise(resolve => {
+setTimeout(() = { resolve("b")
+}, 2000)
+}) 
+let c = new Promise(resolve => {
+‘setTimeout(() => {
+resolve("a")
+}, 1000)
+})
+let d = new Promise(resolve => {
+setTimeout(() => { resolve("d")
+}, 4000)
+})
+	let e = new Promise(resolve => {
+setTimeout(() => { resolve("e")
+}, 5000)
+})
